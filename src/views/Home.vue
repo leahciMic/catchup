cc66838c99a8efb98578eac9935a3bad82501fd4
Michael Leaney <mleaney@tokbox.com>
2017-10-05 15:09:40 +0800

    Minor visual tweaks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-parallax src="1.jpg" class="mb-3" style="height: 400px;">
+    <v-parallax src="main-background.jpg" class="mb-3" style="height: 400px;">
       <v-layout column align-center justify-center>
         <h1 class="white--text mt-5" style="text-shadow: 2px 2px 4px rgba(0,0,0,0.8);">Catchup</h1>
         <h4 class="white--text" style="text-shadow: 2px 2px 4px rgba(0,0,0,0.8);">
@@ -27,33 +27,75 @@
       </v-layout>
     </v-parallax>
 
-    <v-layout>
-      <v-flex xs12 sm3>
-        <v-card>
-          <v-card-media src="2.jpg" height="200px">
-          </v-card-media>
-          <v-card-title primary-title>
-            <div>
-              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-              <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
-            </div>
-          </v-card-title>
-          <v-card-actions>
-            <v-btn flat class="orange--text">Share</v-btn>
-            <v-btn flat class="orange--text">Explore</v-btn>
-          </v-card-actions>
+    <v-layout row v-show="rooms.length">
+      <v-flex xs12 sm4 offset-sm4>
+        <v-card class="ma-2">
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Previous rooms</h3>
+          </div>
+        </v-card-title>
+        <v-card-text>
+          <v-list class="pa-0">
+            <v-list-tile v-for="room in rooms" :key="room">
+              <v-list-tile-action>
+                <v-icon>star</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title v-text="room"></v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-card-text>
         </v-card>
       </v-flex>
     </v-layout>
+    <v-container grid-list>
+    <v-layout row>
+      <v-flex xs12 sm3 offset-sm3>
+        <v-card class="ma-2">
+          <v-card-media src="security.png" height="200px">
+          </v-card-media>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline">World class security</h3>
+              <div>You are safe with us. Audited and vetted by third parties, including financial institutions and industry experts. No user data is ever stored, and your video is encrypted end to end.</div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm3 fill>
+        <v-card class="ma-2">
+          <v-card-media src="speed.jpg" height="200px">
+          </v-card-media>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline">Fast</h3>
+              <div>Our P2P video technology will provide the best possible experience to the device and network you are using. Our video quality automatically adjusts to the constraints of your network and device capabilities in realtime.</div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+    </v-layout>
+    </v-container>
 
   </div>
 </template>
 
 <script>
+  function getRooms() {
+    try {
+      return JSON.parse(localStorage.getItem('rooms'));
+    } catch(e) {
+      return [];
+    }
+  }
   export default {
     data() {
       return {
         roomName: '',
+        rooms: getRooms() || [],
       };
     },
     mounted() {
@@ -61,6 +103,10 @@
     },
     methods: {
       join() {
+        if (this.rooms.indexOf(this.roomName) === -1) {
+          this.rooms.push(this.roomName);
+          localStorage.setItem('rooms', JSON.stringify(this.rooms));
+        }
         this.$router.push(`room/${this.roomName}`);
       }
     }
